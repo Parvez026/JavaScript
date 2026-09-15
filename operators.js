88f150cb2df0783e840946cf02497d7386d258a1
Parvez <parvezalam54802@gmail.com>
2026-09-15 08:31:48 +0530

    practice-string

---
+++ operators.js
@@ -0,0 +1,16 @@
+// console.log(25%4); //remender
+// console.log(2**2);//Squre
+
+// let val=10
+// val++
+// val--
+// val+=20
+// let val2="10"
+// let camp=val2>val //true
+// let camp=val2<val //false
+// console.log(val==val2); //true
+// console.log(val===val2); //false
+
+
+
+
---
+++ string.js
@@ -0,0 +1,32 @@
+// let str="I love Javascript"
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.includes("Javascript"));
+
+// console.log(str.slice(2,6));
+
+// let str="This is mango"
+// console.log(str);
+// console.log(str.replace("mango","Apple"));
+
+// let str="HTML,CSS,JS"
+// console.log(typeof str.split());
+
+let str="I love Javascript"
+// console.log((str.trim()).repeat(5));
+ 
+console.log(str.charAt(0));
+
+
+
+
+
+
+
+
+
+
+
+
+
