a158fd68ac4e45b55a3db1271169a25afae7a861
Parvez <parvezalam54802@gmail.com>
2026-09-16 08:46:29 +0530

    pactice conditions and math operations examples

---
+++ conditions.js
@@ -0,0 +1,58 @@
+// let num = 10;
+// console.log(num);
+// if(num>=0){
+//     console.log("Positive");
+// }
+// else{
+//     console.log("Negative");
+
+// }
+
+// heck whether a number is even or odd.
+
+// if(num%2===0){
+//    console.log("Even");
+// }
+// else{
+//     console.log("odd");
+
+// }
+
+// 4. Find the largest among two numbers.
+// 5. Find the largest among three numbers.
+// 6. Check whether a year is a leap year.
+// 7. Check whether a number is divisible by both 3 and 5.
+// 8. Create a simple grading system:
+// - 90+ → A
+// - 75+ → B
+// - 50+ → C
+// - below 50 → Fail
+
+// let num1 = 10;
+// let num2 = 40;
+// let num3 = 40;
+
+// if (num1 > num2 && num1 > num3) {
+//   console.log("num1 is greater");
+// }
+// else if (num2 > num1 && num2 > num3) {
+//   console.log("num2 is greater");
+// }
+// else if (num3 > num1 && num3 > num2) {
+//   console.log("num3 is greater");
+// }
+// else {
+//   console.log("Some numbers are equal");
+// }
+
+// let year = 2024;
+
+// if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//     console.log("leap year");
+
+// }else console.log("not leap year");
+// num=15
+// if(num%3==0&&num%5==0){
+//  console.log("This number is divisible by 3 and 5 both");
+// }else console.log("not divisible");
+
