b55cb3f45b360537d12f2fc74f88cf2b9c427ce2
Parvez <parvezalam54802@gmail.com>
2026-09-18 09:27:13 +0530

    string manipulation, and conditionals

---
+++ practice3.js
@@ -0,0 +1,40 @@
+// Generate a random OTP of 4 digits.
+
+// let otp=Math.floor(1000+Math.random()*9000)
+// console.log(otp);
+
+// Reverse a 3-letter string manually.
+
+// let str="abc"
+
+// let str1=str[2]+str[1]+str[0]
+// console.log(str1);
+
+
+// let str2=str.split("").reverse().join("")
+// console.log(str2);
+
+// Find the last character of a string.
+
+//   let str="parvez"
+//   let lastChar=str.charAt(str.length-1)
+//   console.log(lastChar);
+  
+// Convert a full name into uppercase initials.
+
+// let name = "parvez alam";
+
+// let initials = name
+//   .split(" ")
+//   .map(word => word[0].toUpperCase())
+//   .join("");
+
+// console.log(initials);
+
+
+// Check whether two strings are equal ignoring case sensitivity.
+
+// let str1="abc"
+// let str2="Abc"
+
+// console.log(str1.toLowerCase()===str2.toLowerCase());
